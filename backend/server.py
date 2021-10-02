8675bc43a8f37e494fdd0df3a752b5b2270b50b5
Charles Averill <46544495+CharlesAverill@users.noreply.github.com>
2021-10-02 16:04:48 -0500

    Add prediction to server.py

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from tensorflow import keras
+import pickle
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods = ['GET'])
+model = keras.models.load_model("/kaggle/working/predictor.h5")
+
+dict = pickle.load(open("/kaggle/working/histories_dict.pkl", "rb"))
+
+@app.route('/', methods = ['POST'])
 def index():
-    return jsonify({
-        'environmentalScore' : 2431,
-        'socialScore': 1343,
-        'governanceScore': 972
+    if request.method == "GET":
+        return jsonify({"nothing_to_display": "Nothing to display!"})
+    else:
+        tag = request.data["tag"]
+        return jsonify({
+            "prediction": model.predict(dict[tag])[0][0]
         })
 
 if __name__ == "__main__":
