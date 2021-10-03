4edbeeb9e136e94c4961104e53615748bbb7b808
Charles Averill <charlesaverill20@gmail.com>
2021-10-02 23:23:57 -0500

    remove model folder

--- model/get_future_esg_rating.py
+++
@@ -1,5 +0,0 @@
-import json
-ticker = "AAPL"
-reuters = {i[:i.index(".")]: j for i, j in json.loads(open("/kaggle/input/reuters/reuters_data.json", "r").read()).items() if "." in i and any(c.isalpha() for c in i[:i.index(".")])}
-prediction = model.predict(d[name][:-1])[0][0]
-future_esg = reuters["AAPL"]["esgScore"]["TR.TRESG"]["score"] * (1 + prediction / 2)
