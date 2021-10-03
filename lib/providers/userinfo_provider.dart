db2341a61d46252d9359967b75cd3d4ce62bbfc5
labusaid <labusaid@gmail.com>
2021-10-03 07:34:11 -0500

    functioning buttons

--- lib/providers/userinfo_provider.dart
+++ lib/providers/userinfo_provider.dart
@@ -2,6 +2,12 @@ import 'package:flutter/cupertino.dart';
 
 class UserInfoProvider with ChangeNotifier {
   Porfolio userPortfolio = Porfolio("Test User", []);
+  String selectedTicker = "";
+
+  void updateTicker(newTicker) {
+    selectedTicker = newTicker;
+    notifyListeners();
+  }
 }
 
 class Porfolio {
